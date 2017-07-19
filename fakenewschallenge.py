c994601a8de7e535bea7cad94d9599291d15ea5b
Javier A. Suarez Rivas <jsuarez@go.olemiss.edu>
2017-07-18 19:18:18 -0500

    Added support for using the correct format on the formating files

--- fakenewschallenge.py
+++ fakenewschallenge.py
@@ -6,13 +6,16 @@ _reddit = praw.Reddit(client_id='U-9whxE5yXShxA', client_secret='OQVVmWYy2rediR-
 
 
 def writeToFiles(stances, body):
-    with open('stances_test_reddit.csv', 'w') as out:
+    with open('./fakeNews/stances_test_reddit.csv', 'w') as out:
         writer = csv.writer(out)
-        stances_header = ['Headline', 'Body ID', 'redditId']
+        stances_header = ['Headline', 'Body ID', 'submissionId', 'commentId']
         writer.writerow(stances_header)
         for stance in stances:
-            writer.writerow([stances[stance]['title'], stances[stance]['Body ID'], stances[stance]['id']])
-    with open('body_test_reddit.csv', 'w') as bout:
+            writer.writerow([stances[stance]['title'],
+                            stances[stance]['Body ID'],
+                            stances[stance]['id'],
+                            stances[stance]['comment_id']])
+    with open('./fakeNews/body_test_reddit.csv', 'w') as bout:
         writer = csv.writer(bout)
         body_header = ['Body ID', 'articleBody']
         writer.writerow(body_header)
@@ -23,28 +26,27 @@ def writeToFiles(stances, body):
 def save_submission_db():
     stances = {}
     body = {}
+    count = 0
     for num, sub in enumerate(_reddit.subreddit('politicalfactchecking').hot(limit=550)):
-        temp_dict = {}
-        temp_dict['id'] = sub.id
-        temp_dict['title'] = sub.title.strip().replace('\n', '')
-        temp_dict['selftext'] = sub.selftext.strip().replace('\n', '')
-        temp_dict['Body ID'] = num
         sub.comments.replace_more(limit=0)
         y = 0
         body_temp = ''
         for comment in sub.comments:
+            temp_dict = {}
+            temp_dict['id'] = sub.id
+            temp_dict['title'] = sub.title.strip().replace('\n', '')
+            temp_dict['selftext'] = sub.selftext.strip().replace('\n', '')
             #print (vars(comment))
-            body_temp += comment.body.strip().replace('\n', '')
             print ('working on {}-{}'.format(num, y))
             y += 1
-        if body_temp:
-            stances[num] = temp_dict
-            body[num] = {'Body ID': num}
-            body[num]['articleBody']= body_temp
-
+            temp_dict['Body ID'] = count
+            temp_dict['comment_id'] = comment.id
+            stances[count] = temp_dict
+            body[count] = {'Body ID': count}
+            body[count]['articleBody']= comment.body.strip().replace('\n', '')
+            count += 1
 
     return stances, body
 
-#update_downs()
 stances, body = save_submission_db()
 writeToFiles(stances, body)
